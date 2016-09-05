0e3d2071e45f6d99b6af587a434631730200951f
nehaj <jhambneha30@gmail.com>
2016-09-05 13:18:24 +0530

    Final app assignment 2

--- app/src/main/java/in/ac/iiitd/psingh/mc16/objectivequiz/HintActivity.java
+++ app/src/main/java/in/ac/iiitd/psingh/mc16/objectivequiz/HintActivity.java
@@ -1,6 +1,5 @@
 package in.ac.iiitd.psingh.mc16.objectivequiz;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
